f189cfde835f0c47f4e81a348aa9ee6168c30e42
Trung Do <dothanhtrung@pm.me>
2024-12-24 17:51:48 +0700

    travel path: Support 3d and circle traveling

--- src/transform.rs
+++ src/transform.rs
@@ -5,11 +5,11 @@ pub struct TransformSupportPlugin;
 
 impl Plugin for TransformSupportPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, moving_2d);
+        app.add_systems(Update, travel);
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 pub struct Destination {
     pub pos: Vec3,
     pub custom_velocity: Option<f32>,
@@ -25,13 +25,15 @@ impl Destination {
 pub struct Movement {
     pub velocity: f32,
     pub des: Vec<Destination>,
+    pub circle: bool,
 }
 
-fn moving_2d(time: Res<Time>, mut query: Query<(&mut Transform, &mut Movement)>) {
+fn travel(time: Res<Time>, mut query: Query<(&mut Transform, &mut Movement)>) {
     for (mut transform, mut movement) in query.iter_mut() {
         if movement.des.is_empty() {
             continue;
         }
+
         let des = movement.des.first().unwrap();
         let velocity = if let Some(custom_v) = des.custom_velocity {
             custom_v
@@ -41,15 +43,20 @@ fn moving_2d(time: Res<Time>, mut query: Query<(&mut Transform, &mut Movement)>)
 
         let mut arrived_x = false;
         let mut arrived_y = false;
+        let mut arrived_z = false;
         let v = velocity * (time.delta().as_millis() as f32);
         let next_stop = movement.des.first().unwrap().pos;
         let x2x1 = next_stop.x - transform.translation.x;
         let y2y1 = next_stop.y - transform.translation.y;
-        let d = (x2x1.powi(2) + y2y1.powi(2)).sqrt();
+        let z2z1 = next_stop.z - transform.translation.z;
+
+        let d_yz = (y2y1.powi(2) + z2z1.powi(2)).sqrt();
+        let d = (d_yz.powi(2) + x2x1.powi(2)).sqrt();
 
         if d * v != 0. {
             let move_x = x2x1 / d * v;
             let move_y = y2y1 / d * v;
+            let move_z = z2z1 / d * v;
 
             if move_x.abs() >= x2x1.abs() {
                 transform.translation.x = next_stop.x;
@@ -64,14 +71,20 @@ fn moving_2d(time: Res<Time>, mut query: Query<(&mut Transform, &mut Movement)>)
             } else {
                 transform.translation.y += move_y;
             }
+
+            if move_z.abs() >= z2z1.abs() {
+                transform.translation.z = next_stop.z;
+                arrived_z = true;
+            } else {
+                transform.translation.z += move_z;
+            }
         }
 
-        if x2x1 == 0. && y2y1 == 0. {
-            arrived_x = true;
-            arrived_y = true;
-        }
-
-        if arrived_x && arrived_y {
+        if arrived_x && arrived_y && arrived_z {
+            if movement.circle {
+                let first_des = movement.as_ref().des.first().unwrap().clone();
+                movement.des.push(first_des);
+            }
             movement.des.remove(0);
         }
     }
