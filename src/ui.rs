887c607466a34beed72e2454b48895cde948f88a
Trung Do <dothanhtrung@pm.me>
2024-12-24 23:24:37 +0700

    Info debug for transform

--- src/ui.rs
+++ src/ui.rs
@@ -1,9 +1,10 @@
 // Copyright 2024 Trung Do <dothanhtrung@pm.me>
 
+use bevy::prelude::With;
 use bevy::{
     app::{App, Plugin, Update},
     math::Vec3,
-    prelude::{Changed, Component, Query, Transform},
+    prelude::{Changed, Component, Query, Text2d, Transform},
     ui::Interaction,
 };
 
@@ -11,7 +12,7 @@ pub struct UiSupportPlugin;
 
 impl Plugin for UiSupportPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, button_press_down_effect);
+        app.add_systems(Update, (button_press_down_effect, update_debug_info));
     }
 }
 
@@ -20,6 +21,10 @@ pub struct PressEffect {
     pub pressing: bool,
     pub orig_scale: Vec3,
 }
+
+#[derive(Component, Default)]
+#[require(Text2d)]
+pub struct TransformInfo;
 
 pub fn button_press_down_effect(
     mut query: Query<(&mut Transform, &mut PressEffect, &Interaction), Changed<Interaction>>,
@@ -47,5 +52,11 @@ pub fn button_press_down_effect(
                 }
             }
         }
+    }
+}
+
+pub fn update_debug_info(mut query: Query<(&mut Text2d, &Transform), With<TransformInfo>>) {
+    for (mut text, transform) in query.iter_mut() {
+        **text = format!("{:?}", transform.translation);
     }
 }
