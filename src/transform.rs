88c956e0762cb62137561627b9da63839795d0ee
Trung Do <dothanhtrung@pm.me>
2024-12-24 23:25:02 +0700

    transform: Only move z if enable feature '3d'

--- src/transform.rs
+++ src/transform.rs
@@ -41,46 +41,65 @@ fn travel(time: Res<Time>, mut query: Query<(&mut Transform, &mut Movement)>) {
             movement.velocity
         };
 
-        let mut arrived_x = false;
-        let mut arrived_y = false;
-        let mut arrived_z = false;
         let v = velocity * (time.delta().as_millis() as f32);
         let next_stop = movement.des.first().unwrap().pos;
         let x2x1 = next_stop.x - transform.translation.x;
         let y2y1 = next_stop.y - transform.translation.y;
-        let z2z1 = next_stop.z - transform.translation.z;
+        let _z2z1 = next_stop.z - transform.translation.z;
 
-        let d_yz = (y2y1.powi(2) + z2z1.powi(2)).sqrt();
-        let d = (d_yz.powi(2) + x2x1.powi(2)).sqrt();
+        let mut arrived_x = x2x1 == 0.;
+        let mut arrived_y = y2y1 == 0.;
+        #[cfg(feature = "3d")]
+        let mut arrived_z = _z2z1 == 0.;
 
-        if d * v != 0. {
+        #[cfg(feature = "3d")]
+        let d_yz_pow2 = y2y1.powi(2) + _z2z1.powi(2);
+        #[cfg(not(feature = "3d"))]
+        let d_yz_pow2 = y2y1.powi(2);
+
+        let d = (d_yz_pow2 + x2x1.powi(2)).sqrt();
+
+        if d != 0. {
             let move_x = x2x1 / d * v;
             let move_y = y2y1 / d * v;
-            let move_z = z2z1 / d * v;
+            let _move_z = _z2z1 / d * v;
 
-            if move_x.abs() >= x2x1.abs() {
-                transform.translation.x = next_stop.x;
-                arrived_x = true;
-            } else {
-                transform.translation.x += move_x;
+            if !arrived_x {
+                if move_x.abs() >= x2x1.abs() {
+                    transform.translation.x = next_stop.x;
+                    arrived_x = true;
+                } else {
+                    transform.translation.x += move_x;
+                }
             }
 
-            if move_y.abs() >= y2y1.abs() {
-                transform.translation.y = next_stop.y;
-                arrived_y = true;
-            } else {
-                transform.translation.y += move_y;
+            if !arrived_y {
+                if move_y.abs() >= y2y1.abs() {
+                    transform.translation.y = next_stop.y;
+                    arrived_y = true;
+                } else {
+                    transform.translation.y += move_y;
+                }
             }
 
-            if move_z.abs() >= z2z1.abs() {
-                transform.translation.z = next_stop.z;
-                arrived_z = true;
-            } else {
-                transform.translation.z += move_z;
+            #[cfg(feature = "3d")]
+            if !arrived_z {
+                if _move_z.abs() >= _z2z1.abs() {
+                    transform.translation.z = next_stop.z;
+                    arrived_z = true;
+                } else {
+                    transform.translation.z += _move_z;
+                }
             }
         }
 
-        if arrived_x && arrived_y && arrived_z {
+        #[cfg(feature = "3d")]
+        let arrived = arrived_x && arrived_y && arrived_z;
+
+        #[cfg(not(feature = "3d"))]
+        let arrived = arrived_x && arrived_y;
+
+        if arrived {
             if movement.circle {
                 let first_des = movement.as_ref().des.first().unwrap().clone();
                 movement.des.push(first_des);
