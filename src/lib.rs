14b7f482411475c1b38e6086f0dc82acbecfc840
Trung Do <dothanhtrung@pm.me>
2025-01-13 17:38:08 +0700

    Provide GameSetting trait for load/save config file

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use bevy::prelude::{Component, Query, Visibility, With};
 
 pub mod transform;
 pub mod ui;
+pub mod setting;
 
 pub fn show_ui<T>(mut mainmenu: Query<&mut Visibility, With<T>>)
 where
