dd856381e0d0bacdb0bfb310f0f33f080f90fd08
Trung Do <dothanhtrung@pm.me>
2025-01-17 00:36:40 +0700

    GameSetting: Implement as trait

--- src/setting.rs
+++ src/setting.rs
@@ -1,41 +1,37 @@
-#[macro_export]
-macro_rules! game_setting {
-    ($struct_name: ident) => {
-        use bevy::asset::ron::ser::{to_string_pretty, PrettyConfig};
-        use std::fs::File;
-        use std::io::Write;
-        use std::path::PathBuf;
-        use bevy::asset::ron::de::from_reader;
+use bevy::asset::ron::de::from_reader;
+use bevy::asset::ron::ser::{to_string_pretty, PrettyConfig};
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
 
-        impl $struct_name {
-            const DEFAULT_CONF: &'static str = "game_setting.conf";
+pub trait GameSetting: Serialize + for<'de> Deserialize<'de> {
+    const DEFAULT_CONF: &'static str = "game_setting.conf";
 
-            fn load(&mut self) -> anyhow::Result<()> {
-                let config_path = PathBuf::from(Self::DEFAULT_CONF);
-                self.load_from(&config_path)
-            }
+    fn load(&mut self) -> anyhow::Result<()> {
+        let config_path = PathBuf::from(Self::DEFAULT_CONF);
+        self.load_from(&config_path)
+    }
 
-            fn load_from(&mut self, config_path: &PathBuf) -> anyhow::Result<()> {
-                let file = File::open(config_path)?;
-                self = from_reader(file)?;
-                Ok(())
-            }
+    fn load_from(&mut self, config_path: &PathBuf) -> anyhow::Result<()> {
+        let file = File::open(config_path)?;
+        *self = from_reader(file)?;
+        Ok(())
+    }
 
-            fn save(&self) -> anyhow::Result<()> {
-                let config_path = PathBuf::from(Self::DEFAULT_CONF);
-                self.save_to(&config_path)
-            }
+    fn save(&self) -> anyhow::Result<()> {
+        let config_path = PathBuf::from(Self::DEFAULT_CONF);
+        self.save_to(&config_path)
+    }
 
-            fn save_to(&self, config_path: &PathBuf) -> anyhow::Result<()> {
-                let pretty = PrettyConfig::default();
-                let ron_str = to_string_pretty(self, pretty)?;
+    fn save_to(&self, config_path: &PathBuf) -> anyhow::Result<()> {
+        let pretty = PrettyConfig::default();
+        let ron_str = to_string_pretty(self, pretty)?;
 
-                if let Some(parent_dir) = config_path.parent() {
-                    std::fs::create_dir_all(parent_dir)?;
-                }
-                let mut file = File::create(config_path)?;
-                file.write_all(ron_str.as_bytes()).map_err(|e| anyhow::anyhow!(e))
-            }
+        if let Some(parent_dir) = config_path.parent() {
+            std::fs::create_dir_all(parent_dir)?;
         }
-    };
+        let mut file = File::create(config_path)?;
+        file.write_all(ron_str.as_bytes()).map_err(|e| anyhow::anyhow!(e))
+    }
 }
