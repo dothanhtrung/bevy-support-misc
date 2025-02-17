98061c2a6a25630a7e72993659621e5c2fe1229a
Trung Do <dothanhtrung@pm.me>
2025-02-17 14:37:37 +0700

    Add common despawn function

--- src/lib.rs
+++ src/lib.rs
@@ -3,28 +3,37 @@
 //! ### Plugin
 //! doc goes here
 
-use bevy::prelude::{Component, Query, Visibility, With};
+use bevy::prelude::{Commands, Component, DespawnRecursiveExt, Entity, Query, Visibility, With};
 
+pub mod easing;
 pub mod save;
 pub mod setting;
 pub mod transform;
 pub mod ui;
-pub mod easing;
 
-pub fn show_ui<T>(mut mainmenu: Query<&mut Visibility, With<T>>)
+pub fn show_ui<T>(mut query: Query<&mut Visibility, With<T>>)
 where
     T: Component,
 {
-    for mut visibility in mainmenu.iter_mut() {
+    for mut visibility in query.iter_mut() {
         *visibility = Visibility::Visible;
     }
 }
 
-pub fn hide_ui<T>(mut mainmenu: Query<&mut Visibility, With<T>>)
+pub fn hide_ui<T>(mut query: Query<&mut Visibility, With<T>>)
 where
     T: Component,
 {
-    for mut visibility in mainmenu.iter_mut() {
+    for mut visibility in query.iter_mut() {
         *visibility = Visibility::Hidden;
+    }
+}
+
+pub fn despawn<T>(mut commands: Commands, query: Query<Entity, With<T>>)
+where
+    T: Component,
+{
+    for e in query.iter() {
+        commands.entity(e).despawn_recursive();
     }
 }
