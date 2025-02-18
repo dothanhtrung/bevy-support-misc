55e83d6b72fe1ebdb99dababc4c2612669598b5e
Trung Do <dothanhtrung@pm.me>
2025-02-18 16:39:11 +0700

    easing: Allow custom ease function

--- src/easing.rs
+++ src/easing.rs
@@ -1,6 +1,8 @@
-
 use bevy::app::App;
-use bevy::prelude::{default, Component, Curve, Deref, DerefMut, EaseFunction, EasingCurve, Entity, Event, Plugin, Query, Res, Time, Transform, Update, Vec3};
+use bevy::prelude::{
+    default, Component, Curve, Deref, DerefMut, EaseFunction, EasingCurve, Entity, Event, EventWriter, Plugin, Quat,
+    Query, Res, Time, Transform, Update, Vec3,
+};
 
 pub struct EasingSupportPlugin;
 
@@ -15,34 +17,37 @@ pub struct EasingEnded(pub Entity);
 
 #[derive(Component, Default)]
 pub struct EasingEffect {
-    target_transform: Transform,
-    orig_transform: Option<Transform>,
-    start_transform:  Option<Transform>,
+    transform_gap: Transform,
+    transform_orig: Option<Transform>,
+    transform_start: Option<Transform>,
     duration_ms: u128,
     elapsed: u128,
+    function: Option<EaseFunction>,
 }
 
 impl EasingEffect {
-
     pub fn orig_transform(&self) -> Option<Transform> {
-        self.orig_transform
+        self.transform_orig
     }
 
-    pub fn popout(scale: Vec3, duration_ms: u128) -> Self {
+    pub fn popout(scale_gap: Vec3, duration_ms: u128) -> Self {
         Self {
-            target_transform: Transform::from_scale(scale),
+            transform_gap: Transform::from_scale(scale_gap),
             duration_ms,
             elapsed: 0,
+            function: Some(EaseFunction::ElasticOut),
             ..default()
         }
     }
 
-    pub fn shake(translation: Vec3, duration_ms: u128) -> Self {
+    pub fn shake(translation_gap: Vec3, origin_translation: Vec3, duration_ms: u128) -> Self {
         Self {
-            target_transform: Transform::from_translation(translation),
+            transform_gap: Transform::from_translation(translation_gap),
+            transform_orig: Some(Transform::from_translation(origin_translation)),
+            transform_start: Some(Transform::from_translation(origin_translation - translation_gap)),
             duration_ms,
             elapsed: 0,
-            ..default()
+            function: Some(EaseFunction::Elastic(50.)),
         }
     }
 
@@ -51,41 +56,56 @@ impl EasingEffect {
         self
     }
 
-    pub fn with_scale(mut self, scale: Vec3) -> Self {
-        self.target_transform.scale = scale;
+    pub fn with_scale_gap(mut self, scale: Vec3) -> Self {
+        self.transform_gap.scale = scale;
         self
     }
 
-    pub fn with_translation(mut self, translattion: Vec3) -> Self {
-        self.target_transform.translation = translattion;
+    pub fn with_translation_gap(mut self, translation: Vec3) -> Self {
+        self.transform_gap.translation = translation;
+        self
+    }
+
+    pub fn with_rotation_gap(mut self, rotation: Quat) -> Self {
+        self.transform_gap.rotation = rotation;
+        self
+    }
+
+    pub fn with_ease_function(mut self, ease_function: EaseFunction) -> Self {
+        self.function = Some(ease_function);
         self
     }
 }
 
-fn easing(mut query: Query<(&mut Transform, &mut EasingEffect)>, time: Res<Time>) {
+fn easing(
+    mut query: Query<(&mut Transform, &mut EasingEffect, Entity)>,
+    mut event: EventWriter<EasingEnded>,
+    time: Res<Time>,
+) {
     let delta = time.delta().as_millis();
-    let f = EasingCurve::new(0.0, 1.0, EaseFunction::ElasticOut);
-    for (mut transform, mut popout) in query.iter_mut() {
-        if popout.orig_transform.is_none() {
-            popout.orig_transform = Some(*transform);
-        }
-        if popout.start_transform.is_none() {
-            popout.start_transform = popout.orig_transform;
-        }
-
-        if popout.elapsed > popout.duration_ms {
+    for (mut transform, mut easing, entity) in query.iter_mut() {
+        if easing.elapsed > easing.duration_ms || easing.function.is_none() {
             continue;
         }
 
-        popout.elapsed += delta;
-        let percent = popout.elapsed as f32 / popout.duration_ms as f32;
-        let rate = f.sample(percent).unwrap_or(1.);
-        if let Some(start) = popout.start_transform {
-            let scale_gap = popout.target_transform.scale - start.scale;
-            let translation_gap = popout.target_transform.translation - start.translation;
+        let f = EasingCurve::new(0.0, 1.0, easing.function.unwrap());
+        if easing.transform_orig.is_none() {
+            easing.transform_orig = Some(*transform);
+        }
+        if easing.transform_start.is_none() {
+            easing.transform_start = easing.transform_orig;
+        }
 
-            transform.scale = start.scale + scale_gap * Vec3::splat(rate);
-            transform.translation = start.translation + translation_gap * Vec3::splat(rate);
+        easing.elapsed += delta;
+        let percent = easing.elapsed as f32 / easing.duration_ms as f32;
+        let rate = f.sample(percent).unwrap_or(1.);
+        if let Some(start) = easing.transform_start {
+            transform.scale = start.scale + easing.transform_gap.scale * Vec3::splat(rate);
+            transform.translation = start.translation + easing.transform_gap.translation * Vec3::splat(rate);
+        }
+
+        if percent >= 1. {
+            event.send(EasingEnded(entity));
         }
     }
 }
