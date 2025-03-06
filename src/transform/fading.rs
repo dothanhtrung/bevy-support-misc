8087f8d27117b9d0de2dbb1a19231f2c85be3523
Trung Do <dothanhtrung@pm.me>
2025-03-06 18:20:53 +0700

    fading: Allow change fade

--- src/transform/fading.rs
+++ src/transform/fading.rs
@@ -40,6 +40,10 @@ impl FadeSupport {
         self.timer.reset();
         self.timer.unpause();
     }
+    
+    pub fn change(&mut self, fade: Fade) {
+        self.fade = fade;
+    }
 }
 
 fn fading(
