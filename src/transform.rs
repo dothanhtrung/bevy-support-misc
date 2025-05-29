25fa4592a0bfb17bfc860859f21adbc6c3f12194
Trung Do <dothanhtrung@pm.me>
2025-05-29 15:56:54 +0700

    Move movement plugin to bevy_movement

--- src/transform.rs
+++ src/transform.rs
@@ -1,14 +1,12 @@
 pub mod fading;
-pub mod movement;
 
-use bevy::prelude::{App, Plugin};
 use crate::transform::fading::FadingSupportPlugin;
-use crate::transform::movement::MovementSupportPlugin;
+use bevy::prelude::{App, Plugin};
 
 pub struct TransformSupportPlugin;
 
 impl Plugin for TransformSupportPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((MovementSupportPlugin, FadingSupportPlugin));
+        app.add_plugins(FadingSupportPlugin);
     }
 }
